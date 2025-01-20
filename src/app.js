ea8e88dde44bbe173f128c7c9504bbf3c7ab45ab
benjamerc <benjamerchan7@gmail.com>
2025-01-20 14:25:02 -0300

    Modify file

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.disable('x-powered-by')
 app.use(cors({ origin: '*' }))
 
 // Motor de plantillas
-app.set("views", path.join(__dirname, "views"))
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.set("view engine", "ejs")
 
 app.use('/public', express.static(path.join(__dirname, '../public')))
